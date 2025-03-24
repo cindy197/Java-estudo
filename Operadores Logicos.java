e380177481ae432fab3981859680101947bb2e1f
cindy197 <136206933+cindy197@users.noreply.github.com>
2025-03-24 16:22:05 -0300

    Operadores Logicos.java

---
+++ Operadores Logicos.java
@@ -0,0 +1,7 @@
+Operadores Lógicos
+ && -> E
+O operador && exige que todas as condições sejam verdadeiras
+ || - > OU
+O operador ou exige que pelo menos uma esteja V
+ ! -> NÃO
+O operador ! inverte a condição
