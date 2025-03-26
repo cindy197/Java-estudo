16ad9fd1d1263a26204e6d28b325447c70e79eb5
cindy197 <136206933+cindy197@users.noreply.github.com>
2025-03-26 15:52:40 -0300

    Switchcase.java

---
+++ Switchcase.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        String dia;
+
+
+        switch (x) {
+            case 1 :
+                dia = "domingo";
+                break;
+            case 2:
+                dia = "segunda";
+                break;
+            case 3:
+                dia = "terça";
+                break;
+            case 4:
+                dia = "quarta";
+                break;
+            case 5:
+                dia = "quinta";
+                break;
+            case 6:
+                dia = "sexta";
+                break;
+            case 7:
+                dia = "sabado";
+                break;
+            default:
+                dia = "invalido";
+                break;
+
+        }
+        System.out.println("Dia da semana: " + dia);
+    }
+}
+
+A Sintaxe do Switch Case é feito da seguinte forma! 
+  switch (expressão) {
+      case valor 1
+         <comando 1> 
+         <comando 2> 
+         break; 
+    case valor2:
+         <comando3>
+         <comando4<
+         break;
+    default:
+         <comando5>
+         <comando6>
+          break;
+ }
